0f239ebe07a1a437fbe89e883a1b1b3ea5e8c86b
25garcial <91680716+25garcial@users.noreply.github.com>
2023-10-24 18:47:15 +0000

    modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
+function randomNum(maxInt){
+    return Math.floor(Math.random()*maxInt);
+}
 class textbox{
+    inputs = window.document.querySelector(".inputs");
+
+constructor(name){
+    
+    var textbox=document.createElement("input");
+    textbox.placeholder=name;
+    textbox.id=toString(randomNum(1000));
+    var submitButton=new button(textbox.id);
 
 
-constructor(){
+    inputs.appendChild(textbox);
+    this.inputs.appendChild(submitButton);
+    return textbox;
 }
 
 }
 
-var textbox = new textbox();
+
+
+
+var input = new textbox();
+submitButton = document.createElement("button");
+submitButton.addEventListener("click", );
